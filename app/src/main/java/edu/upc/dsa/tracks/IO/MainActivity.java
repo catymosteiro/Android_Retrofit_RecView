c994ae6029dbc4033494d47573ece8ff519b281c
catymosteiro <73163835+catymosteiro@users.noreply.github.com>
2022-01-11 13:16:45 +0100

    Minimo2 Actualizado

--- app/src/main/java/edu/upc/dsa/tracks/IO/MainActivity.java
+++ app/src/main/java/edu/upc/dsa/tracks/IO/MainActivity.java
@@ -68,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         public void onFailure(Call<List<Repositorio>> call, Throwable t) {
-
                         }
                     });
                 }
